b0513a8c5ea26505dc30cc9efe0bcbb5a043c4b4
KMBootcamp <kristians.muhametovs@gmail.com>
2016-03-01 16:45:54 +0200

    latest 2nd day

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 var express = require('express');
 var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+
+});
+
+app.listen(3000, function(){
+  console.log("strada");
+});
