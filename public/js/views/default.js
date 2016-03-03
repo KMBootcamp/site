e6a6414ebf34a9550f16ec5b9cba9c561ee913fa
KMBootcamp <kristians.muhametovs@gmail.com>
2016-03-03 14:52:36 +0200

    updating day3

--- public/js/views/default.js
+++ public/js/views/default.js
@@ -3,25 +3,25 @@ define(['jquery', 'underscore', 'backbone', 'mustache', '/js/models/page.js', 't
  function ($, _, Backbone, Mustache, PageModel, pageTemplate, headerTemplate, navigationTemplate) {
   var DefaultView = Backbone.View.extend({
     el: $('.middle'),
-    elHeader: $('.header'),
-    elNavigation: $('.mainItem'),
     data: {},
+    elHeader: $('.header'),
     headerData: {},
+    elNavigation: $('.menu--items'),
     navigationData: {},
 
     initialize: function() {
       this.data = {
         main: {
           title: 'main title',
-          content: 'main content'
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         about: {
           title: 'about title',
-          content: 'about content'
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         contact: {
           title: 'contact title',
-          content: 'contact content'
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
       };
 
@@ -39,15 +39,21 @@ define(['jquery', 'underscore', 'backbone', 'mustache', '/js/models/page.js', 't
 
       this.navigationData = {
         main: {
-          navigation: 'Main'
+          buttons: '<button class="navBtn">Main</button> \n\
+          <button class="navBtn" onclick="window.location.href=\'#about\'">About Me</button>\n\
+          <button class="navBtn" onclick="window.location.href=\'#contact\'">Contant Me</button>'
         },
         about: {
-          navigation: '<a href="#">Main</a>'
+          buttons: '<button class="navBtn" onclick="window.location.href=\'#main\'">Main</button> \n\
+          <button class="navBtn">About Me</button>\n\
+          <button class="navBtn" onclick="window.location.href=\'#contact\'">Contant Me</button>'
         },
         contact: {
-          navigation: '<a href="#">Main</a>'
+          buttons: '<button class="navBtn" onclick="window.location.href=\'#main\'">Main</button> \n\
+          <button class="navBtn" onclick="window.location.href=\'#about\'">About Me</button>\n\
+          <button class="navBtn">Contant Me</button>'
         }
-      };
+      }
 
     },
 
@@ -58,9 +64,8 @@ define(['jquery', 'underscore', 'backbone', 'mustache', '/js/models/page.js', 't
       var compiledHeaderTemplate = Mustache.render ( headerTemplate, this.headerData[pageType]);
       this.elHeader.html( compiledHeaderTemplate );
 
-      var compiledNavigationTemplate = Mustache.render( navigationTemplate, this.navigationData[pageType]);
+      var compiledNavigationTemplate = Mustache.render(navigationTemplate, this.navigationData[pageType]);
       this.elNavigation.html(compiledNavigationTemplate);
-
     }
   });
   return DefaultView;
